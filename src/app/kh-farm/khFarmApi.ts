ca3cfa59b4904b4888542181efa43468c2eb9844
Christian Trinh <trinhc2@mcmaster.ca>
2022-06-28 16:18:43 -0400

    Added moving feature to farm

--- src/app/kh-farm/khFarmApi.ts
+++ src/app/kh-farm/khFarmApi.ts
@@ -34,11 +34,15 @@ export function farmAPI(_els, _setup) {
         previousState: any
         zoom: HTMLElement
         plant: HTMLElement
+        move: HTMLElement
         zoomLevel = 0
         ui: SVGSVGElement
         plotArray = Array.from(Array(20), () => new Array(50))
         colorDictionary: any
         colorCounter: any
+        isDragging = false;
+        dragEnabled = false;
+        dragStart = { x: 0, y: 0 }
 
         constructor(els, setup) {
             self = this
@@ -52,6 +56,7 @@ export function farmAPI(_els, _setup) {
             this.farmGroup = this.els.getElementById("farmGroup") as SVGSVGElement
             this.zoom = this.els.getElementById("zoom") as HTMLElement
             this.plant = this.els.getElementById("plant") as HTMLElement
+            this.move = this.els.getElementById("move") as HTMLElement
             this.ui = this.els.getElementById("ui") as SVGSVGElement
             this.pointerState = this.plant
             this.previousState = this.plant
@@ -102,13 +107,46 @@ export function farmAPI(_els, _setup) {
 
             }
         }
+
         hitTest(e) {
             if (this.pointerState == this.zoom) {
                 this.handleZoom(e)
             }
-            else {
+            else if (this.pointerState == this.plant) {
                 this.plantOrRemoveFlower(e)
             }
+            else if (this.pointerState == this.zoom) {
+                this.dragEnabled = true;
+            }
+        }
+
+        startDrag(e) {
+            if (this.dragEnabled) {
+                var pt = this.els.createSVGPoint()
+                pt.x = e.clientX
+                pt.y = e.clientY
+                pt = pt.matrixTransform(this.els.getScreenCTM().inverse())
+
+                this.dragStart.x = pt.x
+                this.dragStart.y = pt.y
+                this.isDragging = true;
+            }
+        }
+
+        whileDrag(e) {
+            if (this.isDragging) {
+                let baseViewbox = this.els.viewBox["baseVal"]
+                var pt = this.els.createSVGPoint()
+                pt.x = e.clientX
+                pt.y = e.clientY
+                pt = pt.matrixTransform(this.els.getScreenCTM().inverse())
+                gsap.set(this.els, { attr: { viewBox: `${baseViewbox["x"] + (this.dragStart.x - pt.x)} ${baseViewbox["y"] + (this.dragStart.y - pt.y)} ${baseViewbox["width"]} ${baseViewbox["height"]}`} });
+                gsap.to(this.ui, { duration: 0.3, x: baseViewbox["x"] + (this.dragStart.x - pt.x) - 0.5, y: baseViewbox["y"] + (this.dragStart.y - pt.y) - 0.5 });
+            }
+        }
+
+        endDrag(e) {
+            this.isDragging = false;
         }
 
         plantOrRemoveFlower(e) {
@@ -169,7 +207,7 @@ export function farmAPI(_els, _setup) {
 
                     if ((xGridQuadrant % 5) == 0) {
                         rectWidth += 0.5
-                        xVal-=0.4
+                        xVal -= 0.4
                     }
                     if ((xGridQuadrant % 5) == 4) {
                         rectWidth += 0.5
@@ -221,7 +259,7 @@ export function farmAPI(_els, _setup) {
 
                     rectID = `tens${i}-${j}`
 
-                    for (let index = i*2; index < (i*2)+2; index++) {
+                    for (let index = i * 2; index < (i * 2) + 2; index++) {
                         for (let jIndex = 0; jIndex < 50; jIndex++) {
                             if (this.plotArray[index][jIndex]) {
                                 this.colorCounter[this.colorDictionary[this.plotArray[index][jIndex][0]]]--
@@ -235,7 +273,7 @@ export function farmAPI(_els, _setup) {
 
                 gsap.set(rect, { attr: { id: rectID }, x: xVal, y: yVal, width: rectWidth, height: 0, fill: this.flowerColor })
                 this.els.getElementById("fill").appendChild(rect)
-                gsap.to(rect, { height: rectHeight, duration: 1, onComplete: function() {existingElementsToBeDeleted.forEach( e => {e.remove()})}})
+                gsap.to(rect, { height: rectHeight, duration: 1, onComplete: function () { existingElementsToBeDeleted.forEach(e => { e.remove() }) } })
             }
 
 
@@ -255,200 +293,6 @@ export function farmAPI(_els, _setup) {
             }
             element.remove()
         }
-        /*
-        plantOrRemoveFlower(e) {
-            var xVal;
-            var yVal;
-            var rectWidth;
-            var rectHeight;
-            var rectID;
-            var i;
-            var j;
-            var existingElementsToBeDeleted = []
-
-
-            //https://stackoverflow.com/questions/10298658/mouse-position-inside-autoscaled-svg
-            //https://stackoverflow.com/questions/35373882/get-the-global-transform-matrix-of-an-svg-element
-
-            //Calculate original point
-            var pt = this.els.createSVGPoint()
-            pt.x = e.clientX
-            pt.y = e.clientY
-            pt = pt.matrixTransform(this.farmGroup.getScreenCTM().inverse())
-            //console.log(pt)
-
-            //checks bounds so that rects cannot be placed outside of plot
-            if (pt.x < 0) {
-                pt.x = 0
-            }
-            else if (pt.x > this.setup.plotHeight) {
-                pt.x = this.setup.plotHeight - 1
-            }
-            if (pt.y < 0) {
-                pt.y = 0
-            }
-            else if (pt.y > this.setup.plotWidth) {
-                pt.y = this.setup.plotWidth - 1
-            }
-
-            //calculating variables for rect
-            let rect = document.createElementNS(this.svgns, "rect")
-            if (this.isViewingThousands) {//working with thousands
-
-                let innerGridIncrementX = this.plotIncrementWidth / 5;
-                let innerGridIncrementY = this.plotIncrementHeight / 2
-
-                let yGridQuadrant = Math.floor(pt.y / innerGridIncrementY)
-                let xGridQuadrant = Math.floor(pt.x / innerGridIncrementX)
-
-                i = Math.floor(pt.y / innerGridIncrementY);
-                j = Math.floor(pt.x / innerGridIncrementX)
-
-                rectID = `thousands${i}-${j}`
-
-                if (this.pointerState == this.plant) {
-                    rectWidth = ((this.plotIncrementWidth - this.outerLineStrokeWidth) / 5) + 0.2
-                    rectHeight = (this.plotIncrementHeight / 2)
-
-                    xVal = xGridQuadrant * innerGridIncrementX + (this.outerLineStrokeWidth / 2 - ((xGridQuadrant % 5) * this.innerLineStrokeWidth / 2))
-                    //xVal = xGridQuadrant * innerGridIncrementX + this.innerLineStrokeWidth
-
-                    //y value changes depending on 1st or 2nd quad
-                    if (yGridQuadrant % 2 == 0) {
-                        yVal = yGridQuadrant * innerGridIncrementY
-                    }
-                    else {
-                        //if odd then we only have to offset the y by the inner stroke width instead of the outer.
-                        yVal = yGridQuadrant * innerGridIncrementY
-                    }
-
-                    //update colorCounter and update plotArray
-                    if (this.plotArray[i][j]) {
-                        this.colorCounter[this.colorDictionary[this.plotArray[i][j][0]]]--
-                    }
-                    this.plotArray[i][j] = [this.flowerColor, rect]
-                    this.colorCounter[this.colorDictionary[this.plotArray[i][j][0]]]++
-                    
-                }
-                else {//remove a flower if it exists
-                    var element = this.els.getElementById(rectID)
-                    if (element) {
-                        gsap.to(element, { height: 0, duration: 1, onComplete: function () { element.remove() } })
-                        if (this.plotArray[i][j] != null) {
-                            this.colorCounter[this.colorDictionary[this.plotArray[i][j][0]]]--
-                        }
-                        this.plotArray[i][j] = null
-
-                    }
-                }
-            }
-            else if (this.isViewingHundreds) {//working with hundreds
-
-                let xGridQuadrant = Math.floor(pt.x / this.plotIncrementWidth)
-                let yGridQuadrant = Math.floor(pt.y / this.plotIncrementHeight)
-
-                rectWidth = this.plotIncrementWidth
-                rectHeight = this.plotIncrementHeight + 0.5
-
-                xVal = xGridQuadrant * this.plotIncrementWidth
-                yVal = yGridQuadrant * this.plotIncrementHeight
-
-                i = Math.floor(pt.y / this.plotIncrementWidth)
-                j = Math.floor(pt.x / this.plotIncrementHeight)
-
-                rectID = `hundreds${i}-${j}`
-
-                if (this.pointerState == this.plant) {
-                    for (let index = i * 2; index < (i * 2) + 2; index++) {
-                        for (let jIndex = j * 5; jIndex < (j * 5) + 5; jIndex++) {
-                            if (this.plotArray[index][jIndex]) {
-                                this.colorCounter[this.colorDictionary[this.plotArray[index][jIndex][0]]]--
-                                existingElementsToBeDeleted.push(this.plotArray[index][jIndex][1])
-                            }
-                            this.plotArray[index][jIndex] = [this.flowerColor, rect]
-                            this.colorCounter[this.colorDictionary[this.plotArray[index][jIndex][0]]]++
-                        }
-                    }
-                }
-                else {//remove a flower if it exists
-                    var element = this.els.getElementById(rectID)
-                    if (element) {
-                        gsap.to(element, { height: 0, duration: 1, onComplete: function () { element.remove() } })
-                        for (let index = i * 2; index < (i * 2) + 2; index++) {
-                            for (let jIndex = j * 5; jIndex < (j * 5) + 5; jIndex++) {
-                                if (this.plotArray[index][jIndex] != null) {
-                                    this.colorCounter[this.colorDictionary[this.plotArray[index][jIndex][0]]]--
-                                }
-                                this.plotArray[index][jIndex] = null
-                            }
-                        }
-
-                    }
-                }
-            }
-            else {//working with tens
-                let GridQuadrant = Math.floor(pt.x / this.plotIncrementWidth)
-
-                rectWidth = (this.plotIncrementWidth - this.outerLineStrokeWidth);
-                rectHeight = this.setup.plotHeight - (this.outerLineStrokeWidth)
-
-                xVal = GridQuadrant * this.plotIncrementWidth + this.outerLineStrokeWidth / 2
-                yVal = this.outerLineStrokeWidth / 2
-
-                i = Math.floor(pt.x / this.plotIncrementWidth)
-                j = 0
-
-                rectID = `tens${i}-${j}`
-
-                if (this.pointerState == this.plant) {//plant a flower
-                    for (let index = 0; index < 20; index++) {
-                        for (let jIndex = i * 5; jIndex < (i * 5) + 5; jIndex++) {
-                            if (this.plotArray[index][jIndex]) {
-                                this.colorCounter[this.colorDictionary[this.plotArray[index][jIndex][0]]]--
-                                existingElementsToBeDeleted.push(this.plotArray[index][jIndex][1])
-                            }
-
-                            this.plotArray[index][jIndex] = [this.flowerColor, rect]
-                            this.colorCounter[this.colorDictionary[this.plotArray[index][jIndex][0]]]++
-                        }
-                    }
-                }
-                else {//remove flower if it exists
-                    var element = this.els.getElementById(rectID)
-                    if (element) {
-                        gsap.to(element, { height: 0, duration: 1, onComplete: function () { element.remove() } })
-                        for (let index = 0; index < 20; index++) {
-                            for (let jIndex = i * 5; jIndex < (i * 5) + 5; jIndex++) {
-                                if (this.plotArray[index][jIndex] != null) {
-                                    this.colorCounter[this.colorDictionary[this.plotArray[index][jIndex][0]]]--
-                                }
-                                this.plotArray[index][jIndex] = null
-                            }
-                        }
-                    }
-                }
-
-            }
-            //actually create the rect element
-            if (this.pointerState == this.plant) {
-                existingElementsToBeDeleted.concat(gsap.utils.toArray(`#${rectID}`))
-                gsap.set(rect, { attr: { id: rectID }, x: xVal, y: yVal, width: rectWidth, height: 0, fill: this.flowerColor })
-                this.els.getElementById("fill").appendChild(rect)
-                gsap.to(rect, { height: rectHeight, duration: 1, onComplete: this.removeElement, onCompleteParams: [existingElementsToBeDeleted, rectID] })
-            }
-
-            console.log("i = " + i, "j = " + j)
-            console.log("filled: ", this.colorCounter)
-        }
-
-                removeElement(arr, id) {
-            arr.forEach(e => {
-                if (e.id == id) {
-                    e.remove()
-                }
-            })
-        }
-        */
 
         handleZoom(e) {
             let svgBBox = this.els.getBoundingClientRect()
@@ -457,18 +301,18 @@ export function farmAPI(_els, _setup) {
             pt.x = e.clientX
             pt.y = e.clientY
             pt = pt.matrixTransform(this.els.getScreenCTM().inverse())
-            
+
             if (this.zoomLevel == 0) {
                 gsap.to(this.els, { duration: 1, attr: { viewBox: `${(pt.x - 75 - svgBBox.x)} ${(pt.y - 75 - svgBBox.y)} 150 150` }, });
                 this.zoomLevel++
                 this.farmGroup.style.cursor = "zoom-in"
-                gsap.to(this.ui, { x: (pt.x - 75 - svgBBox.x) - 0.5, y: (pt.y - 75 - svgBBox.y) - 0.5, scale: 150/500, duration: 1 },)
+                gsap.to(this.ui, { x: (pt.x - 75 - svgBBox.x) - 0.5, y: (pt.y - 75 - svgBBox.y) - 0.5, scale: 150 / 500, duration: 1 },)
             }
             else if (this.zoomLevel == 1) {
                 gsap.to(this.els, { duration: 1, attr: { viewBox: `${(pt.x - 25 - svgBBox.x)} ${(pt.y - 25 - svgBBox.y)} 50 50` }, });
                 this.zoomLevel++
                 this.farmGroup.style.cursor = "zoom-out"
-                gsap.to(this.ui, { x: (pt.x - 25 - svgBBox.x) - 0.5, y: (pt.y - 25 - svgBBox.y) - 0.5, scale: 50/500, duration: 1 },)
+                gsap.to(this.ui, { x: (pt.x - 25 - svgBBox.x) - 0.5, y: (pt.y - 25 - svgBBox.y) - 0.5, scale: 50 / 500, duration: 1 },)
             }
             else {
                 gsap.to(this.els, { duration: 1, attr: { viewBox: `0 0 500 500` }, });
@@ -503,6 +347,9 @@ export function farmAPI(_els, _setup) {
 
         handlePointerChange(element) {
             this.pointerState = element
+            this.dragEnabled = false
+            this.els.style.cursor = "default"
+
             if (this.pointerState == this.zoom) {
                 if (this.zoomLevel == 2) {
                     this.farmGroup.style.cursor = "zoom-out"
@@ -511,8 +358,12 @@ export function farmAPI(_els, _setup) {
                     this.farmGroup.style.cursor = "zoom-in"
                 }
             }
-            else {
+            else if (this.pointerState == this.plant) {
                 this.farmGroup.style.cursor = "pointer"
+            }
+            else {
+                this.els.style.cursor = "move"
+                this.farmGroup.style.cursor = "move"
             }
             this.previousState.style.fill = "#000000"
             element.style.fill = "#ffffff"
@@ -538,12 +389,12 @@ export function farmAPI(_els, _setup) {
             gsap.set(this.farmGroup, { x: 100, y: 100 })
 
             this.farmGroup.addEventListener("pointerdown", e => { this.hitTest(e) })
-            document.addEventListener("keydown", event => {
-                if (event.keyCode == 32) {
-                    this.handleGridToggle()
-                }
-            })
+
             this.els.getElementById("grid").addEventListener("pointerdown", e => this.handleGridToggle())
+
+            this.els.addEventListener("pointerdown", e => this.startDrag(e))
+            this.els.addEventListener("pointermove", e => this.whileDrag(e))
+            this.els.addEventListener("pointerup", e => this.endDrag(e))
 
             gsap.utils.toArray(".color").forEach(element => element.addEventListener("pointerdown", e => this.handleColorChange(element)))
             gsap.utils.toArray(".pointer").forEach(element => element.addEventListener("pointerdown", e => this.handlePointerChange(element)))
