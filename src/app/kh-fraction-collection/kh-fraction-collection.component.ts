7e86f6f422f79c973902639d0aeabd6122847ee9
Christian Trinh <trinhc2@mcmaster.ca>
2022-09-15 12:42:39 -0400

    implemented more attributes

--- src/app/kh-fraction-collection/kh-fraction-collection.component.ts
+++ src/app/kh-fraction-collection/kh-fraction-collection.component.ts
@@ -13,23 +13,32 @@ export class KhFractionCollectionComponent implements AfterViewInit {
   @Input()
   private backgroundImage: string = "url(https://res.cloudinary.com/dg9cqf9zn/image/upload/v1659473813/barrels2_1_mbrlo2.svg)"
 
+  @Input()
+  private snapTo: string = "20 20"
+
   ngAfterViewInit(): void {
 
     const bar = [{
       "frame": "0 100 200 100",
       "draggable": true,
       "startArray": [0, 1],
-      "lockY": true
+      "lockY": true,
+      "controls": "bottom",
+      "maxDenom": 4
     },
     {
-      "frame": "0 200 100 100",
+      "frame": "0 200 100 200",
       "draggable": true,
       "startArray": [1,1,0,1,0,0,0],
-      "lockX": true
+      "lockX": true,
+      "controls": "right",
+      "maxDenom": 20
     }
     ]
+
     const setup = {
       backgroundImage: this.backgroundImage,
+      snapTo: this.snapTo,
       bar: bar
     } as fractionSetup
 
--- src/app/kh-fraction-collection/khFractionAPI.ts
+++ src/app/kh-fraction-collection/khFractionAPI.ts
@@ -2,17 +2,18 @@ import { gsap, Draggable } from "gsap/all"
 
 export interface fractionSetup{
     backgroundImage: string
-    bar: {"frame": string, "draggable": boolean, "startArray": number[], "lockY": boolean, "lockX": boolean}[]
+    snapTo: string
+    bar: {"frame": string, "draggable": boolean, "startArray": number[], "lockY": boolean, "lockX": boolean, "controls": string, "maxDenom": number}[]
 }
 export function fractionAPI(_els, _setup) {
     let self = {} as fractionClass
 
     class fractionCollection {
         id: number
-        el: {"frame": string, "draggable": boolean, "startArray": number[], "lockY": boolean, "lockX": boolean}
+        setup: {"frame": string, "draggable": boolean, "startArray": number[], "lockY": boolean, "lockX": boolean, "controls": string, "maxDenom": number}
         fractionid: string
         fractionGroup: SVGSVGElement
-        controls: SVGSVGElement
+        controlsElement: SVGSVGElement
         remove: SVGSVGElement
         dragRef: any
         fractionArray:Element[] = []
@@ -23,26 +24,35 @@ export function fractionAPI(_els, _setup) {
         fractionWidth = 250
         fractionHeight = 80
         denom = 1;
-        TL = gsap.timeline()
+        timeline = gsap.timeline()
         uiActive = false
         draggable = true
-        maxSections = 12
+        maxDenom = 12
+        lockX = false
+        lockY = false
+        controls = "bottom"
 
-        constructor(id, el = null) {
+        constructor(id, setup = null) {
             this.id = id
-            this.el = el
+            this.setup = setup
 
-            if (this.el){
-                let frameSplit = el.frame.split(" ")
+            if (this.setup){
+                let frameSplit = setup.frame.split(" ")
                 this.fractionX = parseInt(frameSplit[0])
                 this.fractionY = parseInt(frameSplit[1])
                 this.fractionWidth = parseInt(frameSplit[2])
                 this.fractionHeight = parseInt(frameSplit[3])
 
-                this.draggable = el.draggable || false
+                this.draggable = setup.draggable || false
+
+                this.lockX = setup.lockX || false
+                this.lockY = setup.lockY || false
+                this.controls = setup.controls || "bottom"
+
+                this.maxDenom = setup.maxDenom || 12
             }
 
-            this.fractionFilledInArray = new Array(this.maxSections).fill(0)
+            this.fractionFilledInArray = new Array(this.maxDenom).fill(0)
 
             this.fractionid = "fraction" + id
             this.fractionGroup = document.createElementNS(self.svgns, "g") as SVGSVGElement
@@ -59,35 +69,48 @@ export function fractionAPI(_els, _setup) {
             this.generateFractionCollection()
 
             if (this.draggable){
+
+                let lock;
+                if (this.lockY) {
+                    lock = 'y'
+                }
+                else if (this.lockX){
+                    lock = 'x'
+                }
+                else {
+                    lock = 'x, y'
+                }
                 this.dragRef = Draggable.create(this.fractionGroup, {
-                    type: 'x, y',
+                    type: lock,
                     onDragStart: () => {
                         this.dragged = true
                     },
                     liveSnap: {
                         x: function(value) {
+                            let increment = parseInt(self.setup.snapTo.split(" ")[0])
                             //snap to the closest increment of 20.
-                            return Math.round(value / 20) * 20;
+                            return Math.round(value / increment) * increment;
                         },
                         y: function(value) {
+                            let increment = parseInt(self.setup.snapTo.split(" ")[1])
                             //snap to the closest increment of 20.
-                            return Math.round(value / 20) * 20;
+                            return Math.round(value / increment) * increment;
                         }
                     }
                 })
             }
 
-            if (this.el){
+            if (this.setup){
                 //initialize fraction denominator and numerator
-                for (let i = 0; i < this.el.startArray.length; i++){
+                for (let i = 0; i < this.setup.startArray.length; i++){
 
                     let rectWidth = this.fractionWidth / this.denom
                     for (let i = this.fractionArray.length - 1; i > this.fractionArray.length - this.denom; i--) {
                         gsap.set(this.fractionArray[i], { width: rectWidth })
                         rectWidth += (this.fractionWidth / this.denom)
                     }
-                    if (this.el.startArray[i] == 1){
-                        gsap.set(this.fractionArray[this.maxSections-1 - i], { fill: "rgb(224, 102, 102)" })
+                    if (this.setup.startArray[i] == 1){
+                        gsap.set(this.fractionArray[this.maxDenom-1 - i], { fill: "rgb(224, 102, 102)" })
                         this.fractionFilledInArray[i] = 1
                     }
                     this.denom++;
@@ -96,19 +119,39 @@ export function fractionAPI(_els, _setup) {
 
         }
 
+        setElementPosition(element: Node, x: number, y:number) {
+            gsap.set(element, {attr: {id:this.fractionid + "controls"}, x: x, y: y, display: "hidden"})
+        }
+
         generateFractionCollection() {
-            for (let i = 0; i < this.maxSections; i++) {
+            for (let i = 0; i < this.maxDenom; i++) {
                 let rect = document.createElementNS(self.svgns, "rect")
-                gsap.set(rect, {attr: {numerator: this.maxSections - i - 1}, width: this.fractionWidth, height: this.fractionHeight, x: this.fractionX, y: this.fractionY, fill: "rgb(255, 255, 255)", stroke: "#595959" })
+                gsap.set(rect, {attr: {numerator: this.maxDenom - i - 1}, width: this.fractionWidth, height: this.fractionHeight, x: this.fractionX, y: this.fractionY, fill: "rgb(255, 255, 255)", stroke: "#595959" })
                 this.fractionGroup.appendChild(rect)
                 this.fractionArray.push(rect)
             }
 
-            let controls = document.getElementById("controls").cloneNode(true)
-            let bbox = (document.getElementById("controls") as unknown as SVGSVGElement).getBBox()
-            gsap.set(controls, { attr: {id:this.fractionid + "controls"},x: this.fractionWidth / 2 + this.fractionX - bbox.width / 2, y: this.fractionHeight + this.fractionY + 10, display: "hidden" })
-            this.fractionGroup.appendChild(controls)
-            this.controls = controls as SVGSVGElement
+            let controlEl = self.els.getElementById("controls").cloneNode(true)
+            let bbox = (self.els.getElementById("controls") as unknown as SVGSVGElement).getBBox()
+
+            if (this.controls == "left") {
+                gsap.set(controlEl, { rotation: 90, transformOrigin: "center"})
+                this.setElementPosition(controlEl, this.fractionX - bbox.width + 20, this.fractionY + bbox.height + this.fractionHeight/2 - bbox.width/2)
+                gsap.set(controlEl.childNodes[1], {rotation: 90, transformOrigin: "center"})
+            }
+            else if (this.controls == "right") {
+                gsap.set(controlEl, { rotation: 90, transformOrigin: "center"})
+                this.setElementPosition(controlEl, this.fractionWidth - 20, this.fractionY + bbox.height + this.fractionHeight/2 - bbox.width/2)
+                gsap.set(controlEl.childNodes[1], {rotation: 90, transformOrigin: "center"})
+            }
+            else if (this.controls == "top") {
+                this.setElementPosition(controlEl, this.fractionWidth / 2 + this.fractionX - bbox.width / 2, this.fractionY - bbox.height - 20)
+            }
+            else {
+                this.setElementPosition(controlEl, this.fractionWidth / 2 + this.fractionX - bbox.width / 2, this.fractionHeight + this.fractionY + 20)     
+            }
+            this.fractionGroup.appendChild(controlEl)
+            this.controlsElement = controlEl as SVGSVGElement
 
             //creating remove button
             const remove = document.createElementNS(self.svgns, "g");
@@ -126,7 +169,7 @@ export function fractionAPI(_els, _setup) {
         }
 
         handleIncrease() {
-            if (this.denom < this.maxSections) {
+            if (this.denom < this.maxDenom) {
                 this.denom++;
 
                 let rectWidth = this.fractionWidth / this.denom
@@ -152,11 +195,11 @@ export function fractionAPI(_els, _setup) {
         }
 
         handlePointerDown(e) {
-            if (e.target.parentNode == this.fractionGroup || e.target.parentNode.parentNode == this.fractionGroup || e.target.parentNode.parentNode == this.controls) {
+            if (e.target.parentNode == this.fractionGroup || e.target.parentNode.parentNode == this.fractionGroup || e.target.parentNode.parentNode == this.controlsElement) {
                 self.fractionCollectionGroup.removeChild(this.fractionGroup)
                 self.fractionCollectionGroup.appendChild(this.fractionGroup)
                 if (this.uiActive == false) {
-                    this.controls.style.display = "block"
+                    this.controlsElement.style.display = "block"
                     this.remove.style.display = "block"
                     this.uiActive = true
                 }
@@ -176,7 +219,7 @@ export function fractionAPI(_els, _setup) {
                 }
             }
             else {
-                this.controls.style.display = "none"
+                this.controlsElement.style.display = "none"
                 this.remove.style.display = "none"
                 this.uiActive = false
             }
@@ -193,7 +236,6 @@ export function fractionAPI(_els, _setup) {
                     gsap.set(element, { fill: "rgb(255, 255, 255)" })
                     this.fractionFilledInArray[element.getAttribute("numerator")] = 0
                 }
-                console.log(this.fractionFilledInArray)
 
             }
             this.dragged = false;
@@ -254,9 +296,7 @@ export function fractionAPI(_els, _setup) {
             this.els.getElementById("increase").addEventListener("pointerdown", e => this.handleIncrease())
             this.els.getElementById("decrease").addEventListener("pointerdown", e => this.handleDecrease())
 
-            let label = this.getLabel(16, 2, 4, false)
-            gsap.set(label, { y: 100 })
-            this.els.appendChild(label)
+
             */
 
         }
